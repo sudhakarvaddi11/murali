ca1004a100ba6731d13704e748fc6dd05f398f83
root <root@ip-172-31-47-168.us-east-2.compute.internal>
2018-08-22 06:33:10 +0000

    Changed Hello.sh

--- hello.sh
+++ hello.sh
@@ -1,21 +1,9 @@
-
-#! /bin/bash
-
-##### Install bc - base calculator using command # apt install bc
-
-# Arithmetic operations on Floating Point Numbers
-
-num1=20.5
-num2=5
-
-echo "$num1+$num2" | bc
-echo "$num1-$num2" | bc
-echo "$num1*$num2" | bc
-echo "scale=2; $num1/$num2" | bc
-echo "$num1%$num2" | bc
-
-num3=27
-
-echo "scale=2; sqrt($num3)" | bc -l
-echo "scale=2; 3^3" | bc -l 
+klsdflksdffsd
+sdf
+sdf
+sdf
+sdf
+sdfs
+dfsdfd
+sdf
 
